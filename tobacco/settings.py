3ce45479602649bb4c92d52208661a9958a75d04
meatqw <lexerdxx@gmail.com>
2022-12-08 11:41:38 +0700

    db

--- tobacco/settings.py
+++ tobacco/settings.py
@@ -76,10 +76,19 @@ WSGI_APPLICATION = 'tobacco.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': f'{BASE_DIR}/my.cnf',
+        },
     }
 }
 
